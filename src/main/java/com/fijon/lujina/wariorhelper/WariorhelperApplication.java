5feb4d1b6b8502f1287f2e56d6cab4edf67ff9a3
Fijon <huiqiangxu@126.com>
2019-03-10 22:54:10 +0800

    init project

---
+++ src/main/java/com/fijon/lujina/wariorhelper/WariorhelperApplication.java
@@ -0,0 +1,13 @@
+package com.fijon.lujina.wariorhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WariorhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WariorhelperApplication.class, args);
+	}
+
+}
